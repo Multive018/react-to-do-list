2280947fcec6770d19519355e7e89ebadd14aebe
Sharon Mbeneka <sharonmbeneka8944@gmail.com>
2023-11-04 19:38:29 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { TaskForm } from "./TaskForm";
 import './App.css'
 
 export default function App(){
+  const [newItem, setNewItem] = useState("");
   const [tasks, setTasks] = useState(() => {
     const localValue = localStorage.getItem('ITEMS')
     if (localValue == null) return []
@@ -14,13 +14,16 @@ export default function App(){
     localStorage.setItem('ITEMS', JSON.stringify(tasks))
   }, [tasks])
 
-  function addTask(title){
-    setTasks((currentTask) => {
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    setTasks (currentTask =>{
       return [
-        ...currentTask,
-        { id: crypto.randomUUID(), title, completed: false },
-      ];
+        ...currentTask, { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
     })
+    
+    setNewItem(" ");
   }
 
   function toggleTask(id, completed) {  //enables check(tick) feature
@@ -43,30 +46,48 @@ export default function App(){
   return (
     <>
       <h1>ToDo List</h1>
-      <TaskForm addTasks={addTask} />
+      <div className="body">
+        <form onSubmit={handleSubmit} className="new-item-form">
+          <div>
+            <label htmlFor="item">New Task</label>
+            <input
+              value={newItem}
+              type="text"
+              id="item"
+              placeholder="Enter a task"
+              onChange={(event) => setNewItem(event.target.value)}
+              required
+            />
+          </div>
+          <button className="btn"> Add New Task </button>
+        </form>
+      </div>
       <ul className="list">
-      <h2> My ToDo </h2>
-      {tasks.length === 0 && "No Tasks Added :("}
-      {tasks.map((task) => {//returns an array in the li using the array.map() function
-        return (
-          <li key={task.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={(event) => toggleTask(task.id, event.target.checked)}
-              />
-              {task.title}
-            </label>
-            <button
-              onClick={() => deleteTask(task.id)} //passing a function that calls deleteTask and passes it in the tasks.id (deletes the task)
-              className="delete"
-            >
-              Delete
-            </button>
-          </li>
-        );
-      })}
+        <h2> My ToDo </h2>
+        {tasks.length === 0 && "No Tasks Added :("}
+        {tasks.map((task) => {
+          //returns an array in the li using the array.map() function
+          return (
+            <li key={task.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={(event) =>
+                    toggleTask(task.id, event.target.checked)
+                  }
+                />
+                {task.title}
+              </label>
+              <button
+                onClick={() => deleteTask(task.id)} //passing a function that calls deleteTask and passes it in the tasks.id (deletes the task)
+                className="delete"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
